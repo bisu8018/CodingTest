5ada1eb41076df099082ee3c8efc39e6500da6dc
mac <dean@decompany.io>
2020-11-13 17:13:31 +0900

    start project

--- 3. 힙/디스크 컨트롤러/JS/example.js
+++ 3. 힙/디스크 컨트롤러/JS/example.js
@@ -1,53 +1,36 @@
 function solution(jobs) {
     let result = []
 
-    console.log('jobs', jobs)
-
     const loop = (arr, value, natural) => {
         if (arr.length === 0) return result.push(value)
+        else {
+            for (let i = 0; i < arr.length; ++i) {
+                let tmpArr = arr.slice()
+                let tmpValue = value + (natural + (natural > tmpArr[i][0] ? tmpArr[i][1] - tmpArr[i][0] : tmpArr[i][1] - natural))
+                let tmpNatural = natural > tmpArr[i][0] ? natural +  tmpArr[i][1] :tmpArr[i][0] + tmpArr[i][1]
 
-        let diskArr = arr
+                tmpArr.splice(i, 1)
 
-        console.log('------------------------------')
-        console.log('diskArr : ', diskArr)
-        console.log('value : ', value)
-        console.log('natural : ', natural)
-
-        if (diskArr.length !== 0)
-            for (let i = 0; i < diskArr.length; ++i) {
-                console.log('\n[' + i + ' 번째]')
-
-                let tmpValue = value + (natural + diskArr[i][1] - diskArr[i][0])
-
-                console.log('tmpValue : ', tmpValue)
-                let tmpNatural = natural + diskArr[i][1]
-                let splicedArr = diskArr
-                splicedArr.splice(i, 1)
-
-                console.log('splicedArr : ', splicedArr)
-                console.log()
-
-                return loop(splicedArr, tmpValue, tmpNatural)
+                loop(tmpArr, tmpValue, tmpNatural)
             }
-
-
-        console.log('------------------------------')
+        }
+        return true
     }
 
     let tmpArr = jobs.slice(1, jobs.length)
     loop(tmpArr, jobs[0][1], jobs[0][1])
 
-    let answer = Math.ceil(result.sort((a, b) => a - b)[0] / 3)
-
-    console.log('result : ', result)
-    console.log('answer : ', answer)
-
-    return answer
+    return Math.ceil(result.sort((a, b) => a - b)[0] / 3)
 }
 
 
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
 
 console.log(solution([[0, 3], [1, 9], [2, 6]]) === 9)
+console.log(solution([[0, 3], [4, 3], [8, 3]]) === 3)
+console.log(solution([[0, 5], [6, 1], [6, 2]]) === 3)
+console.log(solution([[0, 5], [6, 2], [6, 1]]) === 3)
+console.log(solution([[0, 5], [2, 2], [5, 3]]) === 5)
+console.log(solution([[0, 5], [2, 2], [4, 2]]) === 5)
